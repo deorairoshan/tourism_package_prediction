814865bff82c0df9f3121ed19f0cf24294501800
Roshan Deorai <deorairoshan@gmail.com>
2026-08-16 17:58:49 +0530

    update tourism_package_prediction/model_building/data_prepare.py

--- tourism_package_prediction/model_building/data_prepare.py
+++ tourism_package_prediction/model_building/data_prepare.py
@@ -1,15 +1,16 @@
+# Importing the libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# loading data from repository
+# Loading data from repository
 df = pd.read_csv("tourism_package_prediction/data/tourism.csv")
 
-# Defining the target variable
+# Define the target variable
 target_col = 'ProdTaken'
 
-# removing unique column Customer Id
+# Removing unique column Customer Id
 df.drop(columns=["CustomerID"], inplace=True)
 
 # Handling missing values for numerical columns
@@ -17,30 +18,24 @@ print("Handling missing values for numerical columns")
 numerical_cols = df.select_dtypes(include=[np.number]).columns
 for col in numerical_cols:
   if df[col].isnull().sum() > 0:
-    df[col].fillna(df[col].median(), inplace=True)
+    df[col] = df[col].fillna(df[col].median())
 
 # Handling missing values for categorical columns
 categorical_cols = df.select_dtypes(include=['object']).columns
 for col in categorical_cols:
   if df[col].isnull().sum() > 0:
-    df[col].fillna(df[col].mode()[0], inplace=True)
+    df[col] = df[col].fillna(df[col].mode()[0])
 
-# Fixing gender inconsistency for 'Fe Male'
+# Fixing gender inconsistency for data - 'Fe Male'
 df['Gender'] = df['Gender'].replace('Fe Male', 'Female')
 
 # Fixing marital status inconsistency for 'Unmarried and Single'
 df['MaritalStatus'] = df['MaritalStatus'].replace('Unmarried', 'Single')
 
-label_encoder = LabelEncoder()
-
-# Encoding categorical columns using label encoder
-for col in categorical_cols:
-  df[col] = label_encoder.fit_transform(df[col].astype(str))
-
 X = df.drop(columns=[target_col])
 y = df[target_col]
 
-# stratify=y keeps the (imbalanced) failure ratio consistent across splits
+# Splitting the dataset into training and test sets 80:20
 Xtrain, Xtest, ytrain, ytest = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
@@ -48,7 +43,7 @@ Xtrain, Xtest, ytrain, ytest = train_test_split(
 print(f"Train data set size: ", Xtrain.shape[0])
 print(f"Test data set size: ", Xtest.shape[0])
 
-# saving the train test data set
+# Saving the train and test data set
 Xtrain.to_csv("Xtrain.csv", index=False)
 Xtest.to_csv("Xtest.csv", index=False)
 ytrain.to_csv("ytrain.csv", index=False)
